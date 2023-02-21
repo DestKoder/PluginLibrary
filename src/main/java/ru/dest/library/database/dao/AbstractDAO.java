1b02eca0e2a3261ca21e26f0fa9dbb686e463a9f
DestKoder <destkoder@gmail.com>
2023-02-21 19:52:11 +0300

    Some fix

--- src/main/java/ru/dest/library/database/dao/AbstractDAO.java
+++ src/main/java/ru/dest/library/database/dao/AbstractDAO.java
@@ -6,9 +6,11 @@ import java.util.List;
 public abstract class AbstractDAO<T> {
 
     public abstract void add(T t);
-    public abstract T getById();
+    public abstract T getById(int id);
     public abstract List<T> getAll();
     public abstract void remove(T t);
+    public abstract void remove(int id);
 
     public void delete(T t){remove(t);}
+    public void delete(int id){remove(id);}
 }
